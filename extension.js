aefcd5c2e37126c6520945ec81c934b7c4d63251
Chironexe <94904712+Chironexe@users.noreply.github.com>
2023-11-11 15:48:56 +0200

    changed porn sites -> adult sites

--- extension.js
+++ extension.js
@@ -1,18 +1,18 @@
-// Checking if the given domain exists within the list of registered porn sites and return true if it does exist.
+// Checking if the given domain exists within the list of registered adult sites and return true if it does exist.
 function checkDomain(domainName) {
-    let isPornSite = false;
+    let isAdultSite = false;
     for (let i = 0; i < domainList.length; i++) {
         if (domainName == domainList[i]) {
-            isPornSite = true;
+            isAdultSite = true;
             break;
         }
     }
-    return isPornSite;
+    return isAdultSite;
 }
 
 let domainName = window.location.origin; // Grabbing the current tabs url
 
-// Calling the checkDomain function to verify if the given website is a porn site.
+// Calling the checkDomain function to verify if the given website is an adult site.
 if (checkDomain(domainName)) {
     window.location.replace("https://www.youtube.com/watch?v=_xcgTCvGbkA")
 }
